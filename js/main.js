05f991b3d71f66a6f9deb11164f049f529ccd4ad
C:\Users\Robik\Desktop\Проекты\React\solmlink <schedamian@gmail.com>
2023-06-08 11:45:27 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const iconMenu = document.querySelector('.header__burger');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
+
+const listMenu = document.querySelector('.menu__list');
+listMenu.addEventListener('click', (e) => {
+  if (e.target.className === 'menu__link') {
+    document.body.classList.remove('_lock');
+    iconMenu.classList.remove('_active');
+    menuBody.classList.remove('_active');
+  }
+});
+
+function ibg() {
+  let ibg = document.querySelectorAll('.ibg');
+  for (let i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+    }
+  }
+}
+ibg();
---
+++ js/slider.js
@@ -0,0 +1,14 @@
+new Swiper('.main__slider', {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  loop: true,
+  speed: 1200,
+  autoplay: {
+    delay: 4000,
+    // disableOnInteraction: false,
+  },
+  // navigation: {
+  //   nextEl: '.reviews__arrow_right',
+  //   prevEl: '.reviews__arrow_left',
+  // },
+});
